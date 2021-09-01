26015445f2b3e18a2b1f4dccb596c145b34ceca8
Lewis Rotchell <rotchell92@gmail.com>
2021-09-01 09:52:06 +0100

    Made svg background on hero section responsive

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,15 +1,8 @@
-import React from 'react'
-import classes from './Header.module.scss'
+import React from "react";
+import classes from "./Header.module.scss";
 
-
-interface HeaderProps {
-
-}
+interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = ({}) => {
-        return (
-        <header className={classes.header}>
-         
-            </header>
-        );
-}
+  return <header className={classes.header}></header>;
+};
