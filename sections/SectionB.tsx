86ebe55468d5211301aa67359788166ff55758d6
Lewis Rotchell <rotchell92@gmail.com>
2021-09-04 16:33:30 +0100

    Finished dektop- section B

--- sections/SectionB.tsx
+++ sections/SectionB.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./SectionB.module.scss";
 import Image from "next/image";
-import PhonesImg from "../public/images/illustration-phones.svg";
+import PhonesImg from "../public/images/phones.svg";
 
 interface SectionBProps {}
 
